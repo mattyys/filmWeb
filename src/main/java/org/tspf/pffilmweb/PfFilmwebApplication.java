1871279cfa7056a3c9713355ca74824e0c5513d1
Matias <matalves@gmail.com>
2024-10-31 16:30:49 -0300

    SE crea esqueleto de proyecto final de Spring tokio

---
+++ src/main/java/org/tspf/pffilmweb/PfFilmwebApplication.java
@@ -0,0 +1,13 @@
+package org.tspf.pffilmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfFilmwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfFilmwebApplication.class, args);
+    }
+
+}
