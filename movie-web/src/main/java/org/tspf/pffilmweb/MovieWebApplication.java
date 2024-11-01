c2bf36d5c1abad161d09d72da93c42c2da3bb3e3
Matias <matalves@gmail.com>
2024-11-01 09:43:49 -0300

    Se crea punto de entrada y docker compose para movie-web

---
+++ movie-web/src/main/java/org/tspf/pffilmweb/MovieWebApplication.java
@@ -0,0 +1,11 @@
+package org.tspf.pffilmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MovieWebApplication.class, args);
+    }
+}
